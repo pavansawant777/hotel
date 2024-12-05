b9fbb161787013c8a0829aeb022fe72cb0aa5ac9
pavansawant777 <126850815+pavansawant777@users.noreply.github.com>
2024-12-05 17:42:37 +0530

    Update Connection.js

--- Admin/Connection.js
+++ Admin/Connection.js
@@ -1,6 +1,6 @@
 mysql=require('mysql')
 conn=mysql.createConnection({
-    host:'localhost',user:"root",password:"",database:"hotels"
+    host:'bkavqvcx1sdsergg5jjr-mysql.services.clever-cloud.com',user:"up2nui2qqxn25tuy",password:"9kUp34pQbzxjTdXPibCR",database:"bkavqvcx1sdsergg5jjr"
 })
 util=require('util')
 execute=util.promisify(conn.query).bind(conn)
